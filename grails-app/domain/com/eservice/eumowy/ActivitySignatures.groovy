5907ad00e2566dfdea5a2e3f5a87d882bee5cd1d
dwalczak <dwalczak@apreel.com>
2013-07-31 10:42:06 +0200

    obiekt domenowy pod mapowanie sygnatur na działania

---
+++ grails-app/domain/com/eservice/eumowy/ActivitySignatures.groovy
@@ -0,0 +1,15 @@
+package com.eservice.eumowy
+
+class ActivitySignatures {
+
+    Activity activity
+
+    Signature signature
+
+    boolean mandatory // not shown on any of list
+
+    Integer numberOfList
+
+    static constraints = {
+    }
+}
