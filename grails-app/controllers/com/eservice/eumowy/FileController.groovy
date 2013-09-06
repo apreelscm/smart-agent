13672df5d2a68835c14726fc28b24fefc92b06d5
mkaczkowski <mkaczkowski@apreel.com>
2013-09-06 14:07:12 +0200

    file controller

---
+++ grails-app/controllers/com/eservice/eumowy/FileController.groovy
@@ -0,0 +1,13 @@
+package com.eservice.eumowy
+
+class FileController {
+
+    def index() {
+        log.info("FileController")
+    }
+
+    def showTmpFile() {
+        log.info("FileController - showTmpFile")
+    }
+
+}
