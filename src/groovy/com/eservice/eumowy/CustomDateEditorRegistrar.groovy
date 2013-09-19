96fede0d197a3f4292640b9007f83620f2f0e0d0
Michal Kniec <mkniec@apreel.com>
2013-09-19 17:56:27 +0200

    Brakujace sql dla slownikow + customowy converter do pol z datami

---
+++ src/groovy/com/eservice/eumowy/CustomDateEditorRegistrar.groovy
@@ -0,0 +1,13 @@
+package com.eservice.eumowy
+import java.text.SimpleDateFormat
+
+import org.springframework.beans.PropertyEditorRegistrar
+import org.springframework.beans.PropertyEditorRegistry
+import org.springframework.beans.propertyeditors.CustomDateEditor
+
+class CustomDateEditorRegistrar implements PropertyEditorRegistrar {
+    public void registerCustomEditors(PropertyEditorRegistry registry) {
+        String dateFormat = 'yyyy-MM-dd'
+        registry.registerCustomEditor(Date, new CustomDateEditor(new SimpleDateFormat(dateFormat), true))
+    }
+}
