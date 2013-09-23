ab29533160dd28626d04fceaafa69e56a70176f1
Kamil Gąsiewski <kgasiewsk@apreel.com>
2013-09-23 10:11:03 +0200

    ProcessCommand - numer rachunku bankowego ze spacjami

--- src/groovy/com/eservice/eumowy/command/ProcessCommand.groovy
+++ src/groovy/com/eservice/eumowy/command/ProcessCommand.groovy
@@ -803,7 +803,7 @@ class ProcessCommand implements Serializable{
         oplPOSBaza(nullable:true, shared: "number")
         obslugaTyp(nullable:false, blank:false)
         obslugaEkonomicznyCena(nullable:true, blank:false, shared: "number") //TODO VERIFY
-        numerRachunkuBankowegoKlienta(nullable:false, blank:false) //TODO  matches: "~|[0-9]{26}") dodac spacje
+        numerRachunkuBankowegoKlienta(nullable:false, blank:false, matches: "~|^[0-9]{2}\\s[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}")
         bankKlienta(nullable:false, blank:false)
         oplataZaUruchomienieDCC(nullable:false, blank:false, shared: "number")
         //nip(nullable:true)
