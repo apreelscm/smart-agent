aa6fa3459f3a8b717cc70ba5c916dec134bf5fb3
Pawel Szkup <pszkup@apreel.com>
2013-09-24 13:57:13 +0200

    EUMOWY-57, EUMOWY-77 - bledy walidacji w nowotworzonych panelach

--- web-app/js/mask.js
+++ web-app/js/mask.js
@@ -1,13 +1,14 @@
 function maskNewPointRefresh() {
-	jQuery("#newPointPanel").find(".bank-account").mask('99 9999 9999 9999 9999 9999 9999');
-	jQuery("#newPointPanel").find(".nip").mask('9999999999');
-	jQuery("#newPointPanel").find(".regon").mask('999999999');
-	jQuery("#newPointPanel").find(".postal-code").mask('99-999');
-	jQuery("#newPointPanel").find(".phone").mask('(99) 999-99-99');
-	jQuery("#newPointPanel").find(".mobile-phone").mask('999-999-999');
-	jQuery("#newPointPanel").find(".fax").mask('(99) 999-99-99');
-	jQuery("#newPointPanel").find(".ip").mask('999.999.999.999');
-	jQuery("#newPointPanel").find(".ph-number").mask('99999');
+    var addNewPointPanelPlaceholder = jQuery("#addNewPointPanelPlaceholder");
+    addNewPointPanelPlaceholder.find(".bank-account").mask('99 9999 9999 9999 9999 9999 9999');
+    addNewPointPanelPlaceholder.find(".nip").mask('9999999999');
+    addNewPointPanelPlaceholder.find(".regon").mask('999999999');
+    addNewPointPanelPlaceholder.find(".postal-code").mask('99-999');
+    addNewPointPanelPlaceholder.find(".phone").mask('(99) 999-99-99');
+    addNewPointPanelPlaceholder.find(".mobile-phone").mask('999-999-999');
+    addNewPointPanelPlaceholder.find(".fax").mask('(99) 999-99-99');
+    addNewPointPanelPlaceholder.find(".ip").mask('0ZZ.0ZZ.0ZZ.0ZZ', {translation:  {'Z': {pattern: /[0-9]/, optional: true}}});
+    addNewPointPanelPlaceholder.find(".ph-number").mask('99999');
 }
 
 jQuery(document).ready(function() {
