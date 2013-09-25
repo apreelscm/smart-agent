070dd2cf530f07ce1edf2d584a0b003b5e3ef13d
dwalczak <dwalczak@apreel.com>
2013-09-25 21:45:17 +0200

    wrzucenie zakomentowanego adresu bazy uat w sieci apreel

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -45,8 +45,9 @@ environments {
             username = "eumowy_app"
             password = "eumowy_app"
             dbCreate = "" // TODO change later for validate
-            //url = "jdbc:oracle:thin:@192.168.3.221:1523:tstcbd"
-            url = "jdbc:oracle:thin:@db-eservice.apreel.lan:1521:cbd01out"
+            url = "jdbc:oracle:thin:@192.168.3.221:1523:tstcbd"
+            //url = "jdbc:oracle:thin:@192.168.3.22:1523:tstcbd" // w sieci apreel
+            //url = "jdbc:oracle:thin:@db-eservice.apreel.lan:1521:cbd01out" // test
         }
     }
     production {
