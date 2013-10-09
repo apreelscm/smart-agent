f3a4994e270e8175dedb1f58c18e00be6c2833d5
dwalczak <dwalczak@apreel.com>
2013-10-09 07:06:30 +0200

    zmiana sql przez eService

--- grails-app/conf/sql/dictionary/getPosTypeComboBox.sql
+++ grails-app/conf/sql/dictionary/getPosTypeComboBox.sql
@@ -2,4 +2,4 @@ select distinct(slownik) as "value" from eumowy.kalkulatortypurzadzen k
 join cbd_adm.cbt_sl_kalk_slownik slo on slo.ksl_kod=k.typ
 where ksl_kod like 'AMORT%'
 and k.medium=:medium
-and ksl_wartosc>=eumowy.getkosztamortyzacji(:nip)
+and ksl_wartosc <= eumowy.getkosztamortyzacji(:nip)
