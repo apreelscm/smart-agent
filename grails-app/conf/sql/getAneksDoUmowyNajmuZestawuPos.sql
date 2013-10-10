66e60878c984d42144398e0c61f0e117499d95e0
Pawel Szkup <pszkup@apreel.com>
2013-10-10 09:19:05 +0200

    eUmowy_ext-239 aktualizacja selektów do dat aneksów

--- grails-app/conf/sql/getAneksDoUmowyNajmuZestawuPos.sql
+++ grails-app/conf/sql/getAneksDoUmowyNajmuZestawuPos.sql
@@ -1,9 +1,16 @@
 SELECT NVL(MAX (umw_data_podpisania),sysdate) as "dataAneksowanejUmowyPos"
-FROM CBD_ADM.cbt_umowy
+FROM cbt_umowy
 WHERE umw_typ = 'ATP'
-      AND umw_kln_id IN
-          (SELECT kln_id
-           FROM CBD_ADM.cbt_klienci
-           WHERE kln_poziom = 'MRC'
-                 AND kln_nip = :nip
-                 AND kln_status = 'Q')
+  AND umw_kln_id IN
+    (SELECT kln_id
+    FROM cbt_klienci m
+    WHERE kln_poziom = 'MRC'
+      AND kln_nip = :nip
+      AND kln_status = 'Q'
+      AND kln_qcards_nr = 1
+      AND EXISTS
+        (SELECT 1
+         FROM cbt_klienci o, cbt_terminale_pos
+         WHERE o.kln_kln_id = m.kln_id
+            AND o.kln_id = tps_kln_id
+            AND tps_status NOT IN ('N', 'C')))
