1b6eb7f691b70bbe7d5cf39d5bf99325ffb2bfc7
mkaczkowski <mkaczkowski@apreel.com>
2013-10-14 13:14:47 +0200

    http://apreel.net/youtrack/issue/EUMOWY-251

--- grails-app/conf/sql/getAneksDoUmowyNajmuZestawuPos.sql
+++ grails-app/conf/sql/getAneksDoUmowyNajmuZestawuPos.sql
@@ -1,16 +1,16 @@
 SELECT NVL(MAX (umw_data_podpisania),sysdate) as "dataAneksowanejUmowyPos"
-FROM cbt_umowy
+FROM CBD_ADM.cbt_umowy
 WHERE umw_typ = 'ATP'
   AND umw_kln_id IN
     (SELECT kln_id
-    FROM cbt_klienci m
+    FROM CBD_ADM.cbt_klienci m
     WHERE kln_poziom = 'MRC'
       AND kln_nip = :nip
       AND kln_status = 'Q'
       AND kln_qcards_nr = 1
       AND EXISTS
         (SELECT 1
-         FROM cbt_klienci o, cbt_terminale_pos
+         FROM CBD_ADM.cbt_klienci o, CBD_ADM.cbt_terminale_pos
          WHERE o.kln_kln_id = m.kln_id
             AND o.kln_id = tps_kln_id
             AND tps_status NOT IN ('N', 'C')))
