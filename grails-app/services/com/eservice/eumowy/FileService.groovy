0e2bd205586f7761768173701c288ea0a30a1193
Michal Kniec <mkniec@apreel.com>
2013-10-17 16:11:58 +0200

    Refaktoring AppParameters

---
+++ grails-app/services/com/eservice/eumowy/FileService.groovy
@@ -0,0 +1,21 @@
+package com.eservice.eumowy
+
+import java.nio.file.Paths
+
+class FileService {
+
+    static transactional = false
+
+    File loadFile(String basePath, String filePath) {
+        def pathToBase = Paths.get(basePath).normalize()
+        def pathToFile = Paths.get(basePath, filePath).normalize()
+
+        if (pathToFile.startsWith(pathToBase)) {
+            def file = pathToFile.toFile()
+            if (file.exists() && file.isFile()) {
+                return file
+            }
+        }
+        return null
+    }
+}
