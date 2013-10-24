df499dc072964ce786af5411865903084f117252
Kamil Gąsiewski <kgasiewsk@apreel.com>
2013-10-24 14:15:39 +0200

    przepuszczanie myslnikow w regexpach

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -83,10 +83,10 @@ grails.cache.config = {
 
 grails.gorm.default.constraints = {
     '*'(nullable: false, blank:true)
-    percentage(matches:'~|-|^(?:100(?:.0(?:0)?)?|\\d{1,2}(?:.\\d{1,2})?)$')
-    number(matches:'~|^(?:[1-9]\\d*|0)?(?:\\.\\d{1,2})?$')
-    number4Precision(matches:'~|^(?:[1-9]\\d*|0)?(?:\\.\\d{1,4})?$')
-    natural(matches:'~|^[0-9]*')
+    percentage(matches:'~|\\-|^(?:100(?:.0(?:0)?)?|\\d{1,2}(?:.\\d{1,2})?)$')
+    number(matches:'~|\\-|^(?:[1-9]\\d*|0)?(?:\\.\\d{1,2})?$')
+    number4Precision(matches:'~|\\-|^(?:[1-9]\\d*|0)?(?:\\.\\d{1,4})?$')
+    natural(matches:'~|\\-|^[0-9]*')
     lettersOnly(matches:'~|^[A-Za-z\\s\\u0104-\\u017c\\u00d3\\u00f3]*')
     alphanumeric(matches:'~|^[A-Za-z0-9\\s.\\-,\\u0104-\\u017c\\u00d3\\u00f3]*')
     date(matches:'~|^\\d{4}$|^\\d{4}[\\-]((((0[13578])|([13578])|(1[02]))[\\-](([1-9])|([0-2][0-9])|(3[01])))|(((0[469])|([469])|(11))[\\-](([1-9])|([0-2][0-9])|(30)))|((2|02)[\\-](([1-9])|([0-2][0-9]))))$')
