07be7b64802df17606ecf18c5509e0636055d022
dwalczak <dwalczak@apreel.com>
2013-10-24 17:05:12 +0200

    zmiana nazw tablespace'y

--- otherResources/sql/1.0.0/00_100_init_dba.sql
+++ otherResources/sql/1.0.0/00_100_init_dba.sql
@@ -1,11 +1,11 @@
-create tablespace EUMOWY_DATA datafile '/oradata/cbd01out/EUMOWY_DATA_01.dbf' size 1G autoextend on next 1G;
-create tablespace EUMOWY_INDX datafile '/oradata/cbd01out/EUMOWY_INDX_01.dbf' size 100M autoextend on next 100M;
+create tablespace EUMOWY datafile '/oradata/cbd01out/EUMOWY_01.dbf' size 1G autoextend on next 1G;
+create tablespace EUMOWY_I datafile '/oradata/cbd01out/EUMOWY_I_01.dbf' size 100M autoextend on next 100M;
 
-create user eumowy identified by V7S1947nK89O DEFAULT TABLESPACE EUMOWY_DATA;
+create user eumowy identified by V7S1947nK89O DEFAULT TABLESPACE EUMOWY;
 ALTER USER "eumowy" PROFILE SERVICE_CBD;
 GRANT RESOURCE TO eumowy;
 GRANT CONNECT TO eumowy;
 
-create user eumowy_app identified by eumowy_app DEFAULT TABLESPACE EUMOWY_DATA;
+create user eumowy_app identified by eumowy_app DEFAULT TABLESPACE EUMOWY;
 ALTER USER "eumowy_app" PROFILE SERVICE_CBD;
 GRANT CONNECT, CREATE SYNONYM TO eumowy_app;
