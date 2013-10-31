19bf5472e9a0c95eb9b85a391c175f79cbf8bddf
dwalczak <dwalczak@apreel.com>
2013-10-31 16:42:51 +0100

    eUmowy_ext-355 nie dziala logowanie na produkcji do tabelki logs
    zmiany w EumowyJDBCAppender pod obsluge DS JNDI

--- src/groovy/com/apache/log4j/jdbc/EumowyJDBCAppender.groovy
+++ src/groovy/com/apache/log4j/jdbc/EumowyJDBCAppender.groovy
@@ -3,25 +3,48 @@ package com.apache.log4j.jdbc
 import org.apache.log4j.Priority
 import org.apache.log4j.jdbc.JDBCAppender
 
+import javax.naming.Context
+import javax.naming.InitialContext
+import javax.naming.NamingException
+import javax.sql.DataSource
+
 /**
- * Created with IntelliJ IDEA.
+ * Log4j appender for audit log
+ *
  * User: mariusz.kaczkowski
  * Date: 30.10.13
  * Time: 13:43
- * To change this template use File | Settings | File Templates.
  */
 class EumowyJDBCAppender extends JDBCAppender {
 
-        protected Object dataSource = null;
+        public EumowyJDBCAppender(Object dataSourceConfig, String name, String sql, Priority threshold) {
+            String jndiName = ((ConfigObject) dataSourceConfig).getProperty("jndiName")?.toString();
+            if (! "[:]".equalsIgnoreCase(jndiName)){
+                try {
+                    Context initialContext = new InitialContext();
+                    Context envCtx = (Context) initialContext.lookup("java:comp/env");
+                    DataSource ds = (DataSource) envCtx.lookup(jndiName);
+                    this.connection(ds.getConnection())
+                }
+                catch (NamingException  e){
+                    throw new IllegalArgumentException(e)
+                }
+            }
+            else {
+                setPropertiesFromConfig((ConfigObject) dataSourceConfig)
+            }
 
-        public EumowyJDBCAppender(Object dataSource, String name, String sql, Priority threshold) {
-            this.dataSource = dataSource;
             this.setName(name);
             this.setSql(sql);
             this.setThreshold(threshold);
-            this.setUser(((ConfigObject)dataSource).getProperty("username").toString());
-            this.setPassword(((ConfigObject)dataSource).getProperty("password").toString());
-            this.setDriver(((ConfigObject)dataSource).getProperty("driverClassName").toString());
-            this.setURL(((ConfigObject)dataSource).getProperty("url").toString());
+
         }
+
+        private void setPropertiesFromConfig(ConfigObject dataSourceConfig){
+            this.setUser(dataSourceConfig.getProperty("username").toString());
+            this.setPassword(dataSourceConfig.getProperty("password").toString());
+            this.setDriver(dataSourceConfig.getProperty("driverClassName").toString());
+            this.setURL(dataSourceConfig.getProperty("url").toString());
+        }
+
 }
