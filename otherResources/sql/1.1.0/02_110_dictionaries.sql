fd0307e45accead775357e9e2a35feeae94d85d3
Pawel Szkup <pszkup@apreel.com>
2013-11-05 16:09:59 +0100

    EUMOWY-150 - Podpis prezesa przykrywa etykiete

--- otherResources/sql/1.1.0/02_110_dictionaries.sql
+++ otherResources/sql/1.1.0/02_110_dictionaries.sql
@@ -5,4 +5,4 @@ insert into EUMOWY.SIGNATURE_PANEL (ID,VERSION,PANEL_ID,SIGNATURE_ID) values ('2
 insert into EUMOWY.ACTIVITY_SIGNATURES (ID,VERSION,ACTIVITY_ID,MANDATORY,NUMBER_OF_LIST,SIGNATURE_ID) values ('51','0','6','1','0','31');
 insert into EUMOWY.ACTIVITY_SIGNATURES (ID,VERSION,ACTIVITY_ID,MANDATORY,NUMBER_OF_LIST,SIGNATURE_ID) values ('52','0','7','1','0','31');
 
-
+update EUMOWY.SUBSCRIPTION_DEFINITION sd set sd.subscriptionx = 330, sd.subscriptiony = 535 where sd.role = 'ZARZAD1' and sd.signature_id = (select s.id from EUMOWY.SIGNATURE s where s.name = 'AT/USU/FDU/4.004/13-05-22')
