f872ec4e1bc38f658503fd8d68db4e35c035613b
dwalczak <dwalczak@apreel.com>
2013-11-08 23:59:01 +0100

    BRUTALNE WYCOFANIE CZESCI REFAKTORINGU PAWLA - nie mozna wejsc na panele -  EUMOWY-300

--- src/groovy/com/eservice/eumowy/command/ProcessCommand.groovy
+++ src/groovy/com/eservice/eumowy/command/ProcessCommand.groovy
@@ -8,6 +8,8 @@ import grails.validation.Validateable
 import org.apache.commons.collections.FactoryUtils
 import org.apache.commons.collections.ListUtils
 
+import java.util.regex.Pattern
+
 /**
  * User: Dominik Walczak
  * Date: 20.08.13 Time: 10:22
@@ -15,7 +17,12 @@ import org.apache.commons.collections.ListUtils
  */
 
 @Validateable
-class ProcessCommand extends BaseCommand {
+class ProcessCommand implements Serializable {
+
+    @Omit()
+    transient def calculatorService
+    @Omit
+    transient def calc
 
     @Omit
     static int MAX_PRICE_GROUP_SIZE = 3
@@ -25,6 +32,67 @@ class ProcessCommand extends BaseCommand {
     // NoSuchFieldException z ProcessService.loadProcessData() przy probie usuniecia zbednej metody
     @Omit
     static def nullableTrueBlankFalse = {/** puste ale potrzebne */}
+
+    @Omit
+    static def atLeastClosure = { value, cmd, errors, property, calcProperty ->
+        def calcValue = cmd.calculatorService.getCalcProperty(cmd.calc, calcProperty)
+
+        log.info("property: ${property}, value: ${value}, cal:${calcValue}")
+
+        //warunek na brak wartości w kalkulatorze lub wartość domyślną w panelu
+        if (DEFAULT_VALUE.equals(value) || !calcValue) {
+            return true
+        }
+
+        def minValue = calcValue?.toString()?.isNumber() ? calcValue.toString().toBigDecimal() : BigDecimal.ZERO
+        def currValue = value?.toString()?.isNumber() ? value.toString().toBigDecimal() : BigDecimal.ZERO
+
+        if (currValue.compareTo(minValue) < 0) {
+            errors.rejectValue(property, "default.atLeast.asCalc",[property] as Object[], "")
+            return false
+        }
+        return true
+    }
+
+    @Omit
+    static def maxLengthClosure = { value, cmd, errors, maxValue, propertyName, message ->
+        if (value.length() > maxValue) {
+            errors.rejectValue(propertyName, message)
+            return false
+        }
+        return true
+    }
+
+    @Omit
+    static def regexpValidationClosure = { value, errors, propertyName, patternStr, message ->
+        log.trace("regexpValidationClosure - propertyName:"+propertyName+" value:"+value + " pattern:"+patternStr)
+
+        if(!value){
+            return true;
+        }
+
+        Pattern pattern = Pattern.compile(patternStr)
+        if (!pattern.matcher(value).matches()){
+            errors.rejectValue(propertyName, message,[value, propertyName] as Object[], "")
+            return false
+        }
+        return true
+    }
+
+    @Omit
+    static def numberValidationClosure = {value, cmd,  errors, propertyName ->
+        regexpValidationClosure.call(value, errors, propertyName, '~|\\-|^(?:[1-9]\\d*|0)?(?:\\.\\d{1,2})?$',"default.validation.number.error")
+    }
+
+    @Omit
+    static def percentageValidationClosure = {value, cmd,  errors, propertyName ->
+        regexpValidationClosure.call(value, errors, propertyName, "~|\\-|^(?:100(?:.0(?:0)?)?|\\d{1,2}(?:.\\d{1,2})?)", "default.validation.number.error")
+    }
+
+    @Omit
+    static def customValidationClosure = {value, cmd,  errors, propertyName, regex ->
+        regexpValidationClosure.call(value, errors, propertyName,regex, "default.validation.regex.error")
+    }
 
     @Omit
     static def checkTerminalNumber = { value, cmd, errors ->
