ba3b29273c15a4e9692ab55947d127fdc85257c0
dwalczak <dwalczak@apreel.com>
2013-11-14 16:06:54 +0100

    tymczasowe wylaczenie pluginu

--- grails-app/conf/GrailsMelodyConfig.groovy
+++ grails-app/conf/GrailsMelodyConfig.groovy
@@ -1,4 +1,5 @@
 /*
+/*
 You can find all detailed parameter usage from
 http://code.google.com/p/javamelody/wiki/UserGuide#6._Optional_parameters
 Any parameter with 'javamelody.' prefix configured in this file will be add as init-param of java melody MonitoringFilter.
@@ -10,7 +11,7 @@ Any parameter with 'javamelody.' prefix configured in this file will be add as i
 /*
 The parameter disabled (false by default) just disables the monitoring.
  */
-//javamelody.disabled = false
+javamelody.disabled = true
 
 /*
 The parameter system-actions-enabled (true by default) enables some system actions.
@@ -21,7 +22,7 @@ The parameter system-actions-enabled (true by default) enables some system actio
 /*
 Turn on Grails Service monitoring by adding 'spring' in displayed-counters parameter.
  */
-javamelody.'displayed-counters' = 'http,sql,error,log,spring,jsp'
+javamelody.'displayed-counters' = 'http,sql,error,log,spring'
 
 
 
