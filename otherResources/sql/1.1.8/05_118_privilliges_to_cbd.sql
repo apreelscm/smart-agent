7c6cfc42bc72ab0e5892607c9b29fbface86ca35
dwalczak <dwalczak@apreel.com>
2013-11-19 23:17:18 +0100

    brakujacy skrypt z grantami pod eUmowy_ext-54 Opieka serwisowa - wypełnianie

---
+++ otherResources/sql/1.1.8/05_118_privilliges_to_cbd.sql
@@ -0,0 +1,4 @@
+-- z poziomu ownera CBD_ADM lub usera majacego odpowiednie uprawnienia
+
+-- eUmowy_ext-54 Opieka serwisowa - wypełnianie
+grant select on CBD_ADM.CBT_SL_KODY_POCZTOWE_PH to EUMOWY_APP;
