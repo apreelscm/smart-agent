72460b004faf0adcc7d2a6b4948a7a5cb5c46723
Pawel Szkup <pszkup@apreel.com>
2013-11-26 10:49:20 +0100

    eUmowy_ext-457 - bledne mapowanie pola na dokument; zmiany na potrzeby M. Adwenta - zapisywanie pola z kalkulatora do ProcessData

--- grails-app/services/com/eservice/eumowy/MapperService.groovy
+++ grails-app/services/com/eservice/eumowy/MapperService.groovy
@@ -19,20 +19,6 @@ class MapperService {
         PdfProcessMapper processMapper = new PdfProcessMapper(calculatorService, calc, new PdfPointMapper(), new PdfPosMapper())
         def data = [:]
         data.putAll(processMapper.mapOnlyProcessData(processInstance))
-        data.putAll(mapProcessCalcToPDFData(calc))
         data
-    }
-
-    private def mapProcessCalcToPDFData(def calc) {
-        def data = [:]
-        if (calculatorService != null){
-            String value = calculatorService.getCalcProperty(calc,'E_LICZBA_MIES_ZWOL_NAJ_1')
-            if (value && value.isInteger()){
-                data.put('oplatyPOSMiesiacNaliczania', [String.valueOf(value.toInteger()+1)] as String[])
-            } else {
-                data.put('oplatyPOSMiesiacNaliczania', ["1"] as String[])
-			}
-        }
-        return data
     }
 }
