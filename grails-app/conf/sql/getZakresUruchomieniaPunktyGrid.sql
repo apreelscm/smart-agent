60b6bbaf365c5401b9263497a253b264f2070cc0
Pawel Szkup <pszkup@apreel.com>
2013-11-26 16:33:18 +0100

    eUmowy_ext-434 - podmiana zapytania o POSy

--- grails-app/conf/sql/getZakresUruchomieniaPunktyGrid.sql
+++ grails-app/conf/sql/getZakresUruchomieniaPunktyGrid.sql
@@ -1,33 +1,28 @@
-SELECT
-         k.kln_id as "id",
-         k.kln_nip as "nip",
-         k.kln_nazwa as "nazwa",
-         a.adr_ulica as "ulica",
-         a.adr_miejscowosc as "miejscowosc",
-         a.adr_nr_budynku as "nr_budynku",
-         adr_kod_pocztowy as "kod_pocztowy",
-         (SELECT   COUNT ( * )
-            FROM   CBD_ADM.cbt_terminale_pos
-           WHERE   tps_status NOT IN ('N', 'C') AND tps_kln_id = k.kln_id)
-            as "liczba_pos"
-  FROM      CBD_ADM.cbt_adresy a
-         JOIN
-            CBD_ADM.cbt_klienci k
-         ON a.adr_kln_id = k.kln_id
- WHERE   k.kln_kln_id IN
-               (SELECT   kln_id
-                  FROM   CBD_ADM.cbt_klienci m
-                 WHERE       kln_poziom = 'MRC'
-                         AND kln_nip =:nip
-                         AND kln_status NOT IN ('N', 'O')
-                         AND kln_qcards_nr = 1
-                         AND EXISTS
-                               (SELECT   1
-                                  FROM   CBD_ADM.cbt_klienci o, CBD_ADM.cbt_terminale_pos
-                                 WHERE       o.kln_kln_id = m.kln_id
-                                         AND o.kln_id = tps_kln_id
-                                         AND tps_status NOT IN ('N', 'C')))
-         AND k.kln_poziom = 'OUT'
-         AND a.adr_rodzaj = 'SGL'
-         AND a.adr_czy_aktywny = 'T'
- ORDER BY k.kln_id
+SELECT k.kln_id     AS "id",
+  k.kln_nip         AS "nip",
+  k.kln_nazwa       AS "nazwa",
+  a.adr_ulica       AS "ulica",
+  a.adr_miejscowosc AS "miejscowosc",
+  a.adr_nr_budynku  AS "nr_budynku",
+  adr_kod_pocztowy  AS "kod_pocztowy",
+  (SELECT COUNT ( * )
+  FROM CBD_ADM.cbt_terminale_pos
+  WHERE tps_status NOT IN ('N', 'C')
+  AND tps_kln_id        = k.kln_id
+  ) AS "liczba_pos"
+FROM CBD_ADM.cbt_adresy a
+JOIN CBD_ADM.cbt_klienci k
+ON a.adr_kln_id     =k.kln_id
+WHERE k.kln_poziom  ='OUT'
+AND a.adr_rodzaj    ='SGL'
+AND kln_nip         =:nip
+AND kln_status NOT IN ('N', 'O')
+AND kln_qcards_nr   = 1
+AND EXISTS
+  (SELECT 1
+  FROM CBD_ADM.cbt_terminale_pos
+  WHERE tps_kln_id    = k.kln_id
+  AND tps_status NOT IN ('N','C')
+  )
+AND a.adr_czy_aktywny = 'T'
+ORDER BY k.kln_id
