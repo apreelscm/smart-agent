196a7b036a0b15caab5040519dae725bef96852e
Pawel Szkup <pszkup@apreel.com>
2013-11-29 16:22:02 +0100

    eUmowy_ext-434 - pobieranie tylko aktywnych punktow.

--- grails-app/conf/sql/dictionary/getCbdPointsComboBox.sql
+++ grails-app/conf/sql/dictionary/getCbdPointsComboBox.sql
@@ -1,23 +1,17 @@
-SELECT
-         k.kln_id as "id",
-         (k.kln_nazwa || ' ' || a.adr_ulica || ' ' || a.adr_nr_budynku || ', ' || a.adr_miejscowosc || ' ' || adr_kod_pocztowy) as "value"
-  FROM      CBD_ADM.cbt_adresy a
-         JOIN
-            CBD_ADM.cbt_klienci k
-         ON a.adr_kln_id = k.kln_id
- WHERE   k.kln_kln_id IN
-               (SELECT   kln_id
-                  FROM   CBD_ADM.cbt_klienci m
-                 WHERE       kln_poziom = 'MRC'
-                         AND kln_nip = :nip
-                         AND kln_status NOT IN ('N', 'O')
-                         AND kln_qcards_nr = 1
-                         AND EXISTS
-                               (SELECT   1
-                                  FROM   CBD_ADM.cbt_klienci o, CBD_ADM.cbt_terminale_pos
-                                 WHERE       o.kln_kln_id = m.kln_id
-                                         AND o.kln_id = tps_kln_id
-                                         AND tps_status NOT IN ('N', 'C')))
-         AND k.kln_poziom = 'OUT'
-         AND a.adr_rodzaj = 'SGL'
-         AND A.adr_czy_aktywny = 'T'
+SELECT k.kln_id AS "id",
+  (k.kln_nazwa || ' ' || a.adr_ulica || ' ' || a.adr_nr_budynku || ', ' || a.adr_miejscowosc || ' ' || adr_kod_pocztowy) AS "value"
+FROM CBD_ADM.cbt_adresy a
+JOIN CBD_ADM.cbt_klienci k
+ON a.adr_kln_id       = k.kln_id
+WHERE k.kln_nip       = :nip
+AND k.kln_status NOT IN ('N', 'O')
+AND k.kln_qcards_nr   = 1
+AND EXISTS
+  (SELECT 1
+  FROM CBD_ADM.cbt_terminale_pos
+  WHERE k.kln_id      = tps_kln_id
+  AND tps_status NOT IN ('N', 'C')
+  )
+AND k.kln_poziom      = 'OUT'
+AND a.adr_rodzaj      = 'SGL'
+AND A.adr_czy_aktywny = 'T'
