03bcaf704c06e6c420ac1949749a12d17478c4a4
Pawel Szkup <pszkup@apreel.com>
2014-01-23 12:22:33 +0100

    eUmowy_ext-496 - brakujace pole

--- grails-app/services/com/eservice/eumowy/PanelService.groovy
+++ grails-app/services/com/eservice/eumowy/PanelService.groovy
@@ -337,6 +337,10 @@ class PanelService {
         cmd.emailDoWysylkiDokumentu = nullify(cmd.emailDoWysylkiDokumentu)
     }
 
+    def setCurrentValue(def data, def calcValue){
+        (data && data.toString().isNumber())? data : calcValue;
+    }
+
     def setAtLeastAs(def data, def calcValue){
         //log.info("data : ${data} , cal : ${calcValue}")
         if(!data || data == "null" || !data.toString().isNumber()) {
@@ -384,7 +388,7 @@ class PanelService {
         cmd.visaPolskaKD2Pr = setAtLeastAs(cmd.visaPolskaKD2Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_322_PROCENT"))
         cmd.visaPolskaKD2St = setAtLeastAs(cmd.visaPolskaKD2St, calculatorService.getCalcProperty(calc,"OPLATA_MSC_322_ZL"))
         cmd.visaPolskaKBPr = setAtLeastAs(cmd.visaPolskaKBPr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_33_PROCENT"))
-        cmd.visaPolskaKBSt = setAtLeastAs(cmd.visaPolskaKBSt,calculatorService.getCalcProperty(calc,"OPLATA_MSC_33_ZL"))
+        cmd.visaPolskaKBSt = setCurrentValue(cmd.visaPolskaKBSt, calculatorService.getCalcProperty(calc,"OPLATA_MSC_33_ZL"))
 
         // 4
         cmd.mastercardEUKKPr = setAtLeastAs(cmd.mastercardEUKKPr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_41_PROCENT"))
@@ -420,7 +424,7 @@ class PanelService {
         cmd.mastercardPolskaKD3Pr = setAtLeastAs(cmd.mastercardPolskaKD3Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_623_PROCENT"))
         cmd.mastercardPolskaKD3St = setAtLeastAs(cmd.mastercardPolskaKD3St, calculatorService.getCalcProperty(calc,"OPLATA_MSC_623_ZL"))
         cmd.mastercardPolskaKBPr = setAtLeastAs(cmd.mastercardPolskaKBPr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_63_PROCENT"))
-        cmd.mastercardPolskaKBSt = (cmd.mastercardPolskaKBSt && cmd.mastercardPolskaKBSt.toString().isNumber())? cmd.mastercardPolskaKBSt : calculatorService.getCalcProperty(calc,"OPLATA_MSC_63_ZL");
+        cmd.mastercardPolskaKBSt = setCurrentValue(cmd.mastercardPolskaKBSt, calculatorService.getCalcProperty(calc,"OPLATA_MSC_63_ZL"))
         cmd.mastercardPolskaM1Pr = setAtLeastAs(cmd.mastercardPolskaM1Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_641_PROCENT"))
         cmd.mastercardPolskaM1St = setAtLeastAs(cmd.mastercardPolskaM1St, calculatorService.getCalcProperty(calc,"OPLATA_MSC_641_ZL"))
         cmd.mastercardPolskaM2Pr = setAtLeastAs(cmd.mastercardPolskaM2Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_642_PROCENT"))
@@ -438,7 +442,7 @@ class PanelService {
         cmd.visaPKOBPKD2Pr = setAtLeastAs(cmd.visaPKOBPKD2Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_722_PROCENT"))
         cmd.visaPKOBPKD2St = setAtLeastAs(cmd.visaPKOBPKD2St, calculatorService.getCalcProperty(calc,"OPLATA_MSC_722_ZL"))
         cmd.visaPKOBPKB3Pr = setAtLeastAs(cmd.visaPKOBPKB3Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_73_PROCENT"))
-        cmd.visaPKOBPKB3St = (cmd.visaPKOBPKB3St && cmd.visaPKOBPKB3St.toString().isNumber())? cmd.visaPKOBPKB3St : calculatorService.getCalcProperty(calc,"OPLATA_MSC_73_ZL");
+        cmd.visaPKOBPKB3St = setCurrentValue(cmd.visaPKOBPKB3St, calculatorService.getCalcProperty(calc,"OPLATA_MSC_73_ZL"))
 
         // 8
         cmd.mastercardPKOBPKK1Pr = setAtLeastAs(cmd.mastercardPKOBPKK1Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_811_PROCENT"))
@@ -454,7 +458,7 @@ class PanelService {
         cmd.mastercardPKOBPKD3Pr = setAtLeastAs(cmd.mastercardPKOBPKD3Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_823_PROCENT"))
         cmd.mastercardPKOBPKD3St = setAtLeastAs(cmd.mastercardPKOBPKD3St, calculatorService.getCalcProperty(calc,"OPLATA_MSC_823_ZL"))
         cmd.mastercardPKOBPKBPr   = setAtLeastAs(cmd.mastercardPKOBPKBPr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_83_PROCENT"))
-        cmd.mastercardPKOBPKBSt = (cmd.mastercardPKOBPKBSt && cmd.mastercardPKOBPKBSt.toString().isNumber())? cmd.mastercardPKOBPKBSt : calculatorService.getCalcProperty(calc,"OPLATA_MSC_83_ZL");
+        cmd.mastercardPKOBPKBSt = setCurrentValue(cmd.mastercardPKOBPKBSt, calculatorService.getCalcProperty(calc,"OPLATA_MSC_83_ZL"))
         cmd.mastercardPKOBPM1Pr = setAtLeastAs(cmd.mastercardPKOBPM1Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_841_PROCENT"))
         cmd.mastercardPKOBPM1St = setAtLeastAs(cmd.mastercardPKOBPM1St, calculatorService.getCalcProperty(calc,"OPLATA_MSC_841_ZL"))
         cmd.mastercardPKOBPM2Pr = setAtLeastAs(cmd.mastercardPKOBPM2Pr, calculatorService.getCalcProperty(calc,"OPLATA_MSC_842_PROCENT"))
