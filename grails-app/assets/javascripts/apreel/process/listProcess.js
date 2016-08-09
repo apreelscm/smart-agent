53924d5fd9a9509d70019e2c992073df68681198
Michał Malanowicz <mmalanowicz@apreel.com>
2016-08-09 15:41:40 +0200

    [eUmowy_ext-869] problem ze zmiana pol 'Data od' oraz 'Data do' w wyszukiwaniu na liscie procesow

--- grails-app/assets/javascripts/apreel/process/listProcess.js
+++ grails-app/assets/javascripts/apreel/process/listProcess.js
@@ -4,12 +4,16 @@
             updateDateTo =  $(".updateDateToDatepicker"),
             createDateFrom =  $(".createDateFromDatepicker"),
             createDateTo =  $(".createDateToDatepicker"),
+            searchDateFrom =  $("#filterDateFromDF"),
+            searchDateTo =  $("#filterDateToDF"),
             reportDialog = $("#reportDialog");
 
         updateDateFrom.datepicker({ dateFormat: 'dd-mm-yy', maxDate: new Date()});
         updateDateTo.datepicker({ dateFormat: 'dd-mm-yy', maxDate: new Date()});
         createDateFrom.datepicker({ dateFormat: 'dd-mm-yy', maxDate: new Date()});
         createDateTo.datepicker({ dateFormat: 'dd-mm-yy', maxDate: new Date()});
+        searchDateFrom.datepicker({ dateFormat: 'dd-mm-yy', maxDate: new Date()});
+        searchDateTo.datepicker({ dateFormat: 'dd-mm-yy', maxDate: new Date()});
 
         updateDateTo.bind('input change',function(e){
             updateDateFrom.datepicker( "option", "maxDate", $(this).datepicker( "getDate" ) );
@@ -25,6 +29,14 @@
 
         createDateFrom.bind('input change',function(e){
             createDateTo.datepicker( "option", "minDate", $(this).datepicker( "getDate" ) );
+        });
+
+        searchDateTo.bind('input change',function(e){
+            searchDateFrom.datepicker( "option", "maxDate", $(this).datepicker( "getDate" ) );
+        });
+
+        searchDateFrom.bind('input change',function(e){
+            searchDateTo.datepicker( "option", "minDate", $(this).datepicker( "getDate" ) );
         });
 
         $("#invalidateCache").on("click", function(e){
