ac389de8271ee5a5d13986157853490ff907da1f
Michał Malanowicz <mmalanowicz@apreel.com>
2023-02-27 18:50:13 +0100

    fix(ESEU-222): mapping representatives and beneficiaries

--- grails-app/services/com/eservice/eumowy/RepresentativeService.groovy
+++ grails-app/services/com/eservice/eumowy/RepresentativeService.groovy
@@ -6,6 +6,10 @@ import com.eservice.eumowy.command.RepresentativeCommand
 import com.eservice.eumowy.enums.options.AcceptorVerification
 import com.eservice.eumowy.enums.options.TelephoneType
 import com.eservice.eumowy.pdfmapper.representative.LegalFormMapper
+import com.google.common.base.Strings
+import groovy.sql.GroovyRowResult
+
+import static com.google.common.base.Strings.isNullOrEmpty
 
 class RepresentativeService {
 
@@ -13,168 +17,108 @@ class RepresentativeService {
     def mapperService
 
     List<RepresentativeCommand> getRepresentativesFromCBD(String nip) {
-        List<RepresentativeCommand> representativeCommands = []
-        def representatives = []
+        List<GroovyRowResult> representatives = cbdService.getReprezentanciFromCbd(nip)
 
-        def representativesList = cbdService.getReprezentanciFromCbd(nip)
+        return representatives.collect { result ->
+            RepresentativeCommand representative = new RepresentativeCommand()
 
-        if (representativesList.size > 0) {
-            representatives.addAll(representativesList)
+            representative.setName(result.imie)
+            representative.setSurname(result.nazwisko)
+            representative.setPosition(result.stanowisko ? mapperService.mapPositionFromCBD(result.stanowisko) : null)
+            representative.setSalutation(result.prefix)
+            representative.setEmail(result.email)
+            representative.setStreet(result.ulica)
+            representative.setStreetTitle(result.ulicaTyp)
+            representative.setCountry(result.kraj)
+            representative.setHouseNumber(result.numerDomu)
+            representative.setFlatNumber(result.numerLokalu)
+            representative.setPostalCode(result.kodPocztowy)
+            representative.setCity(result.miasto)
+            representative.setPostOffice(result.poczta)
+            representative.setPesel(result.pesel)
+            representative.setBirthDate(getDate(result.dataUrodzenia))
+            representative.setCitizenship(result.obywatelstwo)
+            representative.setBirthCountry(result.krajUrodzenia)
+            representative.setDocumentNumber(result.dokumentTozsamosci)
+            representative.setDocumentExpirationDate(getDate(result.dataWaznosciDokumentu))
+            representative.setDocumentIssueDate(getDate(result.dataWydaniaDokumetu))
+            representative.setHasSignedContract(result.czyPodpisalaUmowe ? result.czyPodpisalaUmowe == "T" : null)
+            representative.setLegalFormCBD(result.formaPrawnaID ? LegalFormMapper.mapLegalFormFromCBD(result.formaPrawnaID) : null)
+            representative.setMidCBD(result.mid)
 
-            representatives?.each { result ->
-                RepresentativeCommand representative = new RepresentativeCommand()
-
-                String name = result?.imie ?: ""
-                String surname = result?.nazwisko ?: ""
-                String position = result?.stanowisko ?: ""
-                String salutation = result?.prefix ?: ""
-                String email = result?.email ?: ""
-                String telefonStacjonarny = result?.telefonStac ?: ""
-                String telefonKomorkowy = result?.telefon ?: ""
-                String ulica = result?.ulica ?: ""
-                String kraj = result?.kraj ?: ""
-                String typUlicy = result?.ulicaTyp ?: ""
-                String numerDomu = result?.numerDomu ?: ""
-                String numerLokalu = result?.numerLokalu ?: ""
-                String kodPocztowy = result?.kodPocztowy ?: ""
-                String miasto = result?.miasto ?: ""
-                String poczta = result?.poczta ?: ""
-                String pesel = result?.pesel ?: ""
-                def dataUrodzenia = result?.dataUrodzenia ?: ""
-                String obywatelstwo = mapCountryByCode(result?.obywatelstwo)
-                String krajUrodzenia = mapCountryByCode(result?.krajUrodzenia)
-                String dokumentTozsamosci = result?.dokumentTozsamosci ?: ""
-                def dataWaznosciDokumentu = result?.dataWaznosciDokumentu ?: ""
-                def dataWydaniaDokumentu = result?.dataWydaniaDokumetu ?: ""
-                def czyPodpisalaUmowe = mapHasSignedContract(result?.czyPodpisalaUmowe)
-                int legalFormCode = result?.formaPrawnaID ?: ""
-                String mid = result?.mid ?: ""
-
-                representative.setName(name)
-                representative.setSurname(surname)
-                representative.setPosition(mapperService.mapPositionFromCBD(position))
-                representative.setSalutation(salutation)
-                representative.setEmail(email)
-                representative.setStreet(ulica)
-                representative.setStreetTitle(typUlicy)
-                representative.setCountry(kraj)
-                representative.setHouseNumber(numerDomu)
-                representative.setFlatNumber(numerLokalu)
-                representative.setPostalCode(kodPocztowy)
-                representative.setCity(miasto)
-                representative.setPostOffice(poczta)
-                representative.setIsCBDDataChangedManually(representative?.isCBDDataChangedManually != null ? representative?.isCBDDataChangedManually : true)
-                representative.setPesel(pesel)
-                representative.setBirthDate(dataUrodzenia)
-                representative.setCitizenship(obywatelstwo)
-                representative.setBirthCountry(krajUrodzenia)
-                representative.setDocumentNumber(dokumentTozsamosci)
-                representative.setDocumentExpirationDate(dataWaznosciDokumentu)
-                representative.setDocumentIssueDate(dataWydaniaDokumentu)
-                representative.setHasSignedContract(czyPodpisalaUmowe)
-                representative.setLegalFormCBD(LegalFormMapper.mapLegalFormFromCBD(legalFormCode))
-                representative.setMidCBD(mid as String)
-
-                if (pesel?.length() > 0) {
-                    representative.setVerification(AcceptorVerification.PESEL)
-                } else if (dataUrodzenia?.length() > 0) {
-                    representative.setVerification(AcceptorVerification.BIRTH_DATE)
-                }
-
-                if (telefonKomorkowy?.length() > 0 || telefonStacjonarny.length() > 0) {
-                    if (telefonKomorkowy) {
-                        if (!telefonKomorkowy.contains("-")) {
-                            def maskedPhoneNumber = "";
-                            for (int i = 0; i < telefonKomorkowy.length(); i++) {
-                                if (i == 3 || i == 6) {
-                                    maskedPhoneNumber += "-"
-                                }
-                                maskedPhoneNumber += telefonKomorkowy.charAt(i)
-                            }
-                            representative.setMobilePhone(maskedPhoneNumber)
-                        } else {
-                            representative.setMobilePhone(telefonKomorkowy)
-                        }
-                        representative.setTelephoneType(TelephoneType.MOBILE)
-                    } else if (telefonStacjonarny) {
-                        if (!telefonStacjonarny.contains("-") && telefonStacjonarny.length() == 9) {
-                            def maskedPhoneNumber = "(";
-                            for (int i = 0; i < telefonKomorkowy.length(); i++) {
-                                if (i == 2) {
-                                    maskedPhoneNumber += ") "
-                                }
-                                if (i == 6 || i == 8) {
-                                    maskedPhoneNumber += "-"
-                                }
-                                maskedPhoneNumber += telefonKomorkowy.charAt(i)
-                            }
-                            representative.setLandlinePhone(maskedPhoneNumber)
-                        } else {
-                            representative.setLandlinePhone(telefonStacjonarny)
-                        }
-                        representative.setTelephoneType(TelephoneType.LANDLINE)
-                    } else {
-                        representative.setMobilePhone(null)
-                        representative.setLandlinePhone(null)
-                        representative.setPhoneNumber(null)
-                        representative.setTelephoneType(null)
-                    }
-                }
-
-                representativeCommands.add(representative)
+            if (representative.pesel) {
+                representative.setVerification(AcceptorVerification.PESEL)
+            } else if (representative.birthDate) {
+                representative.setVerification(AcceptorVerification.BIRTH_DATE)
             }
+
+            String telefonStacjonarny = result.telefonStac
+            String telefonKomorkowy = result.telefon
+
+            if (telefonKomorkowy) {
+                if (!telefonKomorkowy.contains("-")) {
+                    def maskedPhoneNumber = "";
+                    for (int i = 0; i < telefonKomorkowy.length(); i++) {
+                        if (i == 3 || i == 6) {
+                            maskedPhoneNumber += "-"
+                        }
+                        maskedPhoneNumber += telefonKomorkowy.charAt(i)
+                    }
+                    representative.setMobilePhone(maskedPhoneNumber)
+                } else {
+                    representative.setMobilePhone(telefonKomorkowy)
+                }
+                representative.setTelephoneType(TelephoneType.MOBILE)
+            } else if (telefonStacjonarny) {
+                if (!telefonStacjonarny.contains("-") && telefonStacjonarny.length() == 9) {
+                    def maskedPhoneNumber = "(";
+                    for (int i = 0; i < telefonKomorkowy.length(); i++) {
+                        if (i == 2) {
+                            maskedPhoneNumber += ") "
+                        }
+                        if (i == 6 || i == 8) {
+                            maskedPhoneNumber += "-"
+                        }
+                        maskedPhoneNumber += telefonKomorkowy.charAt(i)
+                    }
+                    representative.setLandlinePhone(maskedPhoneNumber)
+                } else {
+                    representative.setLandlinePhone(telefonStacjonarny)
+                }
+                representative.setTelephoneType(TelephoneType.LANDLINE)
+            }
+
+            return representative
         }
-        return representativeCommands
     }
 
     List<BeneficiaryCommand> getDaneBeneficjentaRzeczywistego(String nip) {
-        List<BeneficiaryCommand> beneficiaresCommand = []
+        List<GroovyRowResult> beneficiaries = cbdService.getBeneficjenci(nip)
 
-        def beneficiaryList = cbdService.getBeneficjenci(nip)
+        return beneficiaries.collect { result ->
+            BeneficiaryCommand beneficiary = new BeneficiaryCommand()
 
-        beneficiaryList?.each { result ->
-            BeneficiaryCommand representative = new BeneficiaryCommand()
+            beneficiary.setName(result.imie)
+            beneficiary.setSurname(result.nazwisko)
+            beneficiary.setSalutation(result.prefix)
+            beneficiary.setPesel(result.pesel)
+            beneficiary.setBirthDate(getDate(result.dataUrodzenia))
+            beneficiary.setCitizenship(mapCountryByCode(result.obywatelstwo))
+            beneficiary.setMidCBD(result.mid)
 
-            String name = result?.imie ?: ""
-            String surname = result?.nazwisko ?: ""
-            String salutation = result?.prefix ?: ""
-            String pesel = result?.pesel ?: ""
-            def dataUrodzenia = result?.dataUrodzenia ?: ""
-            String obywatelstwo =  mapCountryByCode(result?.obywatelstwo as String)
-
-            if (dataUrodzenia != null && dataUrodzenia != "") {
-                dataUrodzenia = Date.parse("YYYY-mm-dd", dataUrodzenia.toString())
-            }
-
-            representative.setName(name)
-            representative.setSurname(surname)
-            representative.setSalutation(salutation)
-            representative.setPesel(pesel)
-            if (dataUrodzenia != null && dataUrodzenia != "") {
-                representative.setBirthDate(dataUrodzenia as Date)
-            }
-            representative.setCitizenship(obywatelstwo)
-            representative.setIsCBDDataChangedManually(representative?.isCBDDataChangedManually != null ? representative?.isCBDDataChangedManually : true)
-            representative.setMidCBD(result?.mid as String)
-
-            beneficiaresCommand.add(representative)
+            return beneficiary
         }
-
-        return beneficiaresCommand
     }
 
     private mapCountryByCode(String countryCode) {
-        if (countryCode == null) {
-            return null
-        } else {
-            def result = cbdService.getCountryByCountryCode(countryCode)
-            return result?.SKR_NAZWA;
-        }
+        if (countryCode == null) return null
+
+        return cbdService.getCountryByCountryCode(countryCode)?.SKR_NAZWA
     }
 
-    private static Boolean mapHasSignedContract(result) {
-        if (result == null) {
-            return null
-        } else return result == "T"
+    private Date getDate(String dateAsString) {
+        if (isNullOrEmpty(dateAsString)) return null
+
+        return Date.parse("YYYY-mm-dd", dateAsString)
     }
 }
