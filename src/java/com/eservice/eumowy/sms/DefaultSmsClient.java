60ed55190871e9a66cac3fb9b95856b952edbad5
Michał Knieć <mkniec@apreel.com>
2025-07-18 09:30:51 +0200

    fix(ESEU-139): SMS json serialization

--- src/java/com/eservice/eumowy/sms/DefaultSmsClient.java
+++ src/java/com/eservice/eumowy/sms/DefaultSmsClient.java
@@ -1,16 +1,22 @@
 package com.eservice.eumowy.sms;
 
 import com.eservice.eumowy.MobilePhoneNumber;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import groovy.json.JsonBuilder;
 import org.apache.log4j.Logger;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import static java.lang.Boolean.parseBoolean;
 
@@ -38,6 +44,26 @@ class Sms {
     String sender;
     String source;
     Boolean partner;
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public Boolean getPartner() {
+        return partner;
+    }
 
     @Override
     public String toString() {
@@ -79,13 +105,20 @@ public class DefaultSmsClient implements SmsClient {
     private void sendSms(Sms sms) {
         URI uri = UriComponentsBuilder
                 .fromUriString(properties.uri)
-                .pathSegment("/api/send")
+                .pathSegment("api", "send")
                 .build().encode().toUri();
 
-        ResponseEntity<String> resp = restTemplate.postForEntity(uri, sms, String.class);
-        log.info("Sent " + resp.getBody());
-        if (resp.getStatusCode() != HttpStatus.OK) {
-            throw new ErrorSendingSmsException();
+        try {
+            HttpEntity<String> request = new HttpEntity<String>(toRequestBody(sms), headers);
+            ResponseEntity<String> resp = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
+            log.info("Sent " + resp.getBody());
+            if (resp.getStatusCode() != HttpStatus.OK) {
+                throw new ErrorSendingSmsException();
+            }
+        } catch (HttpStatusCodeException e) {
+            log.error("Status " + e.getStatusCode().toString() + " response:\n" + e.getResponseBodyAsString());
+        } catch (Exception e) {
+            log.error("Error sending SMS", e);
         }
     }
 
@@ -101,12 +134,19 @@ public class DefaultSmsClient implements SmsClient {
 
     private HttpHeaders newHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Content-Type", "application/json;charset=UTF-8");
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         return headers;
     }
 
     private RestTemplate newRestTemplate() {
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        messageConverters.add(converter);
         return new RestTemplate();
+    }
+
+    private String toRequestBody(Sms sms) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(sms);
     }
 }
